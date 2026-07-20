c3b381b143a296d0fdfcdbed8f26c4b57daa12b1
Silky <silky@localhost.com>
2026-07-20 12:11:32 +0200

    Create initial user controller and routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return "hello AKMP";
+});
+
+Route::get('/users', [UserController::class, 'index']);
